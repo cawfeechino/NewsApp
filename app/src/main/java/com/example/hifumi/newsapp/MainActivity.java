ec9cdae792ca772246744253b2cb12881a18ac20
rumiel <shiamatsu@gmail.com>
2018-10-27 22:24:46 -0700

    textview version stays here

--- app/src/main/java/com/example/hifumi/newsapp/MainActivity.java
+++ app/src/main/java/com/example/hifumi/newsapp/MainActivity.java
@@ -1,22 +1,30 @@
 package com.example.hifumi.newsapp;
 
-import android.net.Network;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.hifumi.newsapp.utilities.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    private TextView newsView;
     private String results;
+    private Toolbar tbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.newsView);
+        tbar = findViewById(R.id.tbar);
+        setSupportActionBar(tbar);
+        newsView = findViewById(R.id.textView);
         NewsTask task = new NewsTask();
         task.execute();
     }
@@ -34,8 +42,23 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e){
                 e.printStackTrace();
             }
-            textView.setText(results);
             return null;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.getNews:
+                newsView.setText(results);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
